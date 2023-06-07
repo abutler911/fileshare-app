1c1179f0b29a37dd75ebac6f388abb91230cdc9d
Andrew Butler <abutler911@gmail.com>
2023-06-07 09:14:59 -0600

    fixed styling

--- public/code.js
+++ public/code.js
@@ -11,10 +11,18 @@
   document
     .querySelector("#sender-start-con-btn")
     .addEventListener("click", function () {
-      let joinID = generateID;
+      let joinID = generateID();
       document.querySelector("#join-id").innerHTML = `
         <b>Room ID</b>
         <span>${joinID}</span>
         `;
+      socket.emit("sender-join", {
+        uid: joinID,
+      });
     });
+  socket.on("init", function (uid) {
+    receiverID = uid;
+    document.querySelector(".join-screen").classList.remove("active");
+    document.querySelector(".fs-screen").classList.add("active");
+  });
 })();
